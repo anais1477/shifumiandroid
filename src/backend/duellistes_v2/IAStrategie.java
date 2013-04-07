851a139559ad3f27530ea7ea8ed4401202d2b6a0
Anais <anais7714@gmail.com>
2013-04-07 18:50:39 +0200

    Restructuration of directories

--- src/backend/duellistes_v2/IAStrategie.java
+++
@@ -1,5 +0,0 @@
-package backend.duellistes_v2;
-
-public interface IAStrategie {
-	public Attack choiceAttack();
-}
--- src/backend/duellistes_v2/Result.java
+++
@@ -1,7 +0,0 @@
-package backend.duellistes_v2;
-
-public enum Result {
-	VICTORY_1,
-	VICTORY_2,
-	IN_PROGRESS;
-}
